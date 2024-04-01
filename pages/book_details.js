747b3d8eeca59d680ad443fdede49999d6c8c162
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-04-01 01:52:44 -0400

    Adding srif reports and fixed vulnerability

--- pages/book_details.js
+++ pages/book_details.js
@@ -2,31 +2,47 @@ let async = require('async');
 let Book = require('../models/book');
 let BookInstance = require('../models/bookinstance');
 
+// Function to get book details by ID
 function get_book(id) {
-    if (typeof id !== "string") {
-        return ({status: "error"});
+    // Ensure id is a valid ObjectId
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        return null; // Return null if id is not a valid ObjectId
     }
-    return Book.findOne({'_id': {$eq: id}}).populate('author');
+    return Book.findOne({'_id': id}).populate('author');
 }
 
+// Function to get book instance details by book ID
 function get_book_dtl(id) {
-  return BookInstance
-          .find({ 'book': id })
-          .select('imprint status');
+    // Ensure id is a valid ObjectId
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        return null; // Return null if id is not a valid ObjectId
+    }
+    return BookInstance.find({ 'book': id }).select('imprint status');
 }
 
-exports.show_book_dtls = async (res, id) => {
-  const results = await Promise.all([get_book(id).exec(), get_book_dtl(id).exec()])
-  try {
-    let book = await results[0];
-    let copies = await results[1];
-    res.send({
-      title: book.title,
-      author: book.author.name,
-      copies: copies,
-    });
-  }
-  catch(err) {
-    res.send(`Book ${id} not found`);
-  } 
+// Controller function to show book details
+exports.show_book_dtls = async (req, res) => {
+    const id = req.params.id; // Assuming id is passed in URL parameters
+    try {
+        // Fetch book details and book instance details concurrently using Promise.all
+        const [book, copies] = await Promise.all([
+            get_book(id),
+            get_book_dtl(id)
+        ]);
+        
+        // Check if book exists
+        if (!book) {
+            return res.status(404).send(`Book ${id} not found`);
+        }
+
+        // Send response with book details
+        res.send({
+            title: book.title,
+            author: book.author.name,
+            copies: copies
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
 }
